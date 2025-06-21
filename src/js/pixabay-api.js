1e605bbe7f702dea7079b80af2b2249ea779fca1
Oleg Ganus <oleg110770@gmail.com>
2025-06-21 14:52:12 +0300

    edit home work

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,27 +2,22 @@ import axios from "axios";
 
 const MY_API_KEY = "50762825-7fe49127b3d94f6c93c99dfe1";
 
-export function getImagesByQuery(query,currentPage=1) {
+export async function getImagesByQuery(query, currentPage = 1) {
     const imageType = "photo";
     const orientation = "horizontal";
     const safesearch = true;
     const perPage = 15;
-    
 
-
-    return axios.get('https://pixabay.com/api/', {
+    const response = await axios.get('https://pixabay.com/api/', {
         params: {
             key: MY_API_KEY,
             q: query,
             image_type: imageType,
             orientation: orientation,
-            safesearch: safesearch,
+            ssafesearch: safesearch,
             per_page: perPage,
             page: currentPage,
-        }
-    })
-    .then(response => {
-       
-        return response.data;
+        },
     });
-    }
+    return response.data;
+}
